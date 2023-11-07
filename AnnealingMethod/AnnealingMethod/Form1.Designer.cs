39241ffa593ea6779e4387b6eeb4857a4adfdc21
darazonova <darazonova@6E67>
2023-11-07 19:35:11 +0300

    Добавьте файлы проекта.

---
+++ AnnealingMethod/AnnealingMethod/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace AnnealingMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericNodes = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNodes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericNodes
+            // 
+            this.numericNodes.Location = new System.Drawing.Point(32, 45);
+            this.numericNodes.Margin = new System.Windows.Forms.Padding(6);
+            this.numericNodes.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericNodes.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericNodes.Name = "numericNodes";
+            this.numericNodes.Size = new System.Drawing.Size(240, 31);
+            this.numericNodes.TabIndex = 4;
+            this.numericNodes.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericNodes.ValueChanged += new System.EventHandler(this.numericNodes_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number of cities";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Matrix";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1455, 738);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericNodes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericNodes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericNodes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AnnealingMethod/AnnealingMethod/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnnealingMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x, x1, y, y1;
+        List<List<NumericUpDown>> MatrixNodes = new List<List<NumericUpDown>>();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericNodes_ValueChanged(object sender, EventArgs e)
+        {
+            x = 400; //start of matrix elements
+            y = 500;
+            Random random = new Random();
+            y1 = y;
+
+            for (int i = 0; i < MatrixNodes.Count(); i++) //clean matrix
+                for (int j = 0; j < MatrixNodes[i].Count(); j++)
+                    this.Controls.Remove(MatrixNodes[i][j]);
+            MatrixNodes.Clear();
+
+            for (int i = 1; i < numericNodes.Value; i++)
+            {
+                x1 = x;
+                MatrixNodes.Add(new List<NumericUpDown>());
+                for (int j = 0; j < numericNodes.Value - 1; j++)
+                {
+                    if (i > j)
+                    {
+                        MatrixNodes[i - 1].Add(new NumericUpDown());
+                        MatrixNodes[i - 1].Add(new NumericUpDown());
+                        MatrixNodes[i - 1][j].Size = new Size(37, 20);
+                        MatrixNodes[i - 1][j].Location = new Point(y1, x1);
+                        MatrixNodes[i - 1][j].Minimum = random.Next(1, 100);
+                        Controls.Add(MatrixNodes[i - 1][j]);
+                        x1 -= 20;
+                    }
+                }
+                y1 -= 30;
+
+            }
+
+        
+       
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
